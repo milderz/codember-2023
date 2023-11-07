f5543ffed02c5240584956713a89f867e4aa5d31
milderz <mzunax@gmail.com>
2023-11-07 10:57:20 -0600

    Add challenge01 solution

--- challenge01/challenge01.js
+++ challenge01/challenge01.js
@@ -1,1 +1,31 @@
+const getChallengeData = async () => {
+  const res = await fetch("https://codember.dev/data/message_01.txt");
+  const data = await res.text();
+  return data;
+};
 
+const data = getChallengeData();
+
+const countWords = async (text) => {
+  const wordsCount = {};
+  const string = await text;
+  const result = [];
+
+  const words = string.split(" ");
+  for (let word of words) {
+    word = word.trim();
+    if (!wordsCount[word]) {
+      wordsCount[word] = 1;
+    } else {
+      wordsCount[word]++;
+    }
+  }
+
+  for (const word in wordsCount) {
+    result.push(`${word}${wordsCount[word]}`);
+  }
+
+  console.log(result.join(""));
+};
+
+countWords(data);
